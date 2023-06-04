10a8771b313f5d55588d29c2fee32ac4424c981d
Denis Popkov <d.popkov@ltst.ru>
2023-06-05 05:37:33 +0600

    fix: add serializer ext

--- src/jvmMain/kotlin/ru/popkov/coursework/AnySerializer.kt
+++ src/jvmMain/kotlin/ru/popkov/coursework/AnySerializer.kt
@@ -5,9 +5,7 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonDecoder
-import kotlinx.serialization.json.JsonEncoder
+import kotlinx.serialization.json.*
 import kotlinx.serialization.serializer
 
 inline fun <reified T : Any> anySerializer(): AnySerializer<T> {
@@ -29,5 +27,52 @@ class AnySerializer<T : Any>(private val dataSerializer: KSerializer<T>) : KSeri
         return jsonDecoder.decodeJsonElement().let {
             Json.decodeFromJsonElement(dataSerializer, it)
         }
+    }
+}
+
+private fun Any?.toJsonPrimitive(): JsonPrimitive {
+    return when (this) {
+        null -> JsonNull
+        is JsonPrimitive -> this
+        is Boolean -> JsonPrimitive(this)
+        is Number -> JsonPrimitive(this)
+        is String -> JsonPrimitive(this)
+        else -> throw Exception("${this::class}")
+    }
+}
+
+private fun JsonPrimitive.toAnyValue(): Any? {
+    val content = this.content
+    return when {
+        this.isString -> content
+        content.equals("null", ignoreCase = true) -> null
+        content.equals("true", ignoreCase = true) -> true
+        content.equals("false", ignoreCase = true) -> false
+        content.toIntOrNull() != null -> content.toInt()
+        content.toLongOrNull() != null -> content.toLong()
+        content.toDoubleOrNull() != null -> content.toDouble()
+        else -> throw Exception("content：$content")
+    }
+}
+
+private fun Any?.toJsonElement(): JsonElement {
+    return when (this) {
+        null -> JsonNull
+        is JsonElement -> this
+        is Boolean -> JsonPrimitive(this)
+        is Number -> JsonPrimitive(this)
+        is String -> JsonPrimitive(this)
+        is Iterable<*> -> JsonArray(this.map { it.toJsonElement() })
+        is Map<*, *> -> JsonObject(this.map { it.key.toString() to it.value.toJsonElement() }.toMap())
+        else -> throw Exception("${this::class}=${this}}")
+    }
+}
+
+private fun JsonElement.toAnyOrNull(): Any? {
+    return when (this) {
+        is JsonNull -> null
+        is JsonPrimitive -> toAnyValue()
+        is JsonObject -> this.map { it.key to it.value.toAnyOrNull() }.toMap()
+        is JsonArray -> this.map { it.toAnyOrNull() }
     }
 }
